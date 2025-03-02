5adbe663f0d5bb16200c1e60ec548e983d6ca48a
kagamaru <01a17124@gmail.com>
2025-03-02 16:13:41 +0900

    :recycle: mocksからのimportにエラーハンドリング追加

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,10 @@
 export async function register() {
     if (process.env.NEXT_RUNTIME === "nodejs" && process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
-        const { initMocks } = await import("@/mocks");
-        initMocks();
+        try {
+            const { initMocks } = await import("@/mocks");
+            initMocks();
+        } catch {
+            console.error("Function not imported");
+        }
     }
 }
