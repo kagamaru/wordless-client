88a9f2e3f199c566262cc0f4001524c9941d98e7
kagamaru <01a17124@gmail.com>
2025-03-23 15:02:28 +0900

    :bug: Maximum update depth エラーの対策

--- src/components/template/ProviderTemplate.tsx
+++ src/components/template/ProviderTemplate.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ConfigProvider } from "antd";
-import { useMemo } from "react";
+import { useMemo, useRef } from "react";
 import { AuthProvider } from "react-oidc-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -14,6 +14,7 @@ export function ProviderTemplate({
     const cognitoClientId = process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID ?? undefined;
     const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI ?? undefined;
     const queryClient = new QueryClient();
+    const providerTemplateRef = useRef<React.ReactElement | undefined>(undefined);
 
     if (!authority || !cognitoClientId || !redirectUri) {
         throw new Error("必要な環境変数が設定されていません");
@@ -30,20 +31,24 @@ export function ProviderTemplate({
         [authority, cognitoClientId, redirectUri]
     );
 
-    return (
-        <AuthProvider {...cognitoAuthConfig}>
-            <QueryClientProvider client={queryClient}>
-                <ConfigProvider
-                    theme={{
-                        token: {
-                            colorPrimary: "#7829cc"
-                        }
-                    }}
-                >
-                    {children}
-                </ConfigProvider>
-            </QueryClientProvider>
-            ;
-        </AuthProvider>
-    );
+    if (!providerTemplateRef.current) {
+        providerTemplateRef.current = (
+            <AuthProvider {...cognitoAuthConfig}>
+                <QueryClientProvider client={queryClient}>
+                    <ConfigProvider
+                        theme={{
+                            token: {
+                                colorPrimary: "#7829cc"
+                            }
+                        }}
+                    >
+                        {children}
+                    </ConfigProvider>
+                </QueryClientProvider>
+                ;
+            </AuthProvider>
+        );
+    }
+
+    return providerTemplateRef.current;
 }
