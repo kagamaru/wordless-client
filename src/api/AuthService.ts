734bd3324a5e5f997fea4a15c4315ef470a7a481
kagamaru <01a17124@gmail.com>
2025-04-09 08:24:07 +0900

    :sparkles: アクセストークンをAPI呼び出し時に渡す

--- src/api/AuthService.ts
+++ src/api/AuthService.ts
@@ -1,55 +1,22 @@
-import {
-    AuthenticationResultType,
-    CognitoIdentityProviderClient,
-    GetUserCommand,
-    InitiateAuthCommand,
-    SignUpCommand
-} from "@aws-sdk/client-cognito-identity-provider";
+import { restApiRequest } from "./restApiUtility/restApiRequest";
 
-const client = new CognitoIdentityProviderClient({
-    region: process.env.NEXT_PUBLIC_AWS_REGION ?? ""
-});
+const restApiUrl = process.env.NEXT_PUBLIC_REST_API_URL ?? "";
 
 export class AuthService {
-    public signin = async (credentials: {
-        email: string;
-        password: string;
-    }): Promise<AuthenticationResultType | undefined> => {
+    public signin = async (credentials: { email: string; password: string }): Promise<void> => {
         try {
-            const command = new InitiateAuthCommand({
-                AuthFlow: "USER_PASSWORD_AUTH",
-                ClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID ?? "",
-                AuthParameters: {
-                    USERNAME: credentials.email,
-                    PASSWORD: credentials.password
+            await restApiRequest(
+                restApiUrl + "login/",
+                "POST",
+                {
+                    ...credentials
+                },
+                {
+                    headers: {
+                        credentials: "include"
+                    }
                 }
-            });
-
-            const response = await client.send(command);
-            return response.AuthenticationResult;
-        } catch (error) {
-            throw error;
-        }
-    };
-
-    public signup = async (email: string, password: string): Promise<void> => {
-        try {
-            const command = new SignUpCommand({
-                ClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID!,
-                Username: email,
-                Password: password,
-                UserAttributes: [{ Name: "email", Value: email }]
-            });
-
-            await client.send(command);
-        } catch (error) {
-            throw error;
-        }
-    };
-
-    public getUser = async (accessToken: string) => {
-        try {
-            return await client.send(new GetUserCommand({ AccessToken: accessToken }));
+            );
         } catch (error) {
             throw error;
         }
--- src/api/restApiUtility/restApiRequest.ts
+++ src/api/restApiUtility/restApiRequest.ts
@@ -1,6 +1,12 @@
-export async function restApiRequest<T>(url: string, options?: RestApiRequestOption): Promise<T> {
+export async function restApiRequest<T>(
+    url: string,
+    method: "GET" | "POST",
+    data?: Object,
+    options?: RestApiRequestOption
+): Promise<T> {
     try {
-        const response = await fetchWithTimeout(url, options);
+        const response =
+            method === "GET" ? await fetchWithTimeout(url, options) : await postWithTimeout(url, data, options);
         return response as T;
     } catch (error) {
         throw error;
@@ -23,8 +29,41 @@ async function fetchWithTimeout(url: string, options?: RestApiRequestOption, tim
     }
 }
 
+async function postWithTimeout(
+    url: string,
+    data: unknown,
+    options?: RestApiRequestOption,
+    timeout = 5000
+): Promise<Response> {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            signal: controller.signal,
+            headers: {
+                "Content-Type": "application/json",
+                ...(options?.headers || {})
+            },
+            body: JSON.stringify(data),
+            ...options
+        });
+
+        clearTimeout(timeoutId);
+
+        if (!response.ok) {
+            throw new Error(JSON.stringify(await response.json()));
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+}
+
 type RestApiRequestOption = {
-    method: string;
-    headers: Record<string, string>;
-    body: string;
+    method?: string;
+    headers?: Record<string, string>;
+    body?: string;
 };
