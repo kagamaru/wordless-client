2cfa025b3884f31cf61772a09cf03127d6aca794
kagamaru <01a17124@gmail.com>
2025-04-20 18:44:57 +0900

    ✨ Emojiの型定義を更新し、EmojiCategoryを追加。関連するコンポーネントでの使用を修正。

--- src/@types/Emoji.ts
+++ src/@types/Emoji.ts
@@ -1,14 +1,27 @@
 import { EmojiString } from "@/@types/EmojiString";
 
 export type Emoji = {
+    emojiCategory: EmojiCategory;
     emojiType: EmojiType;
     emojiId: EmojiString;
     emojiJapaneseId: string;
     url?: string;
 };
 
-export enum EmojiType {
+export enum EmojiCategory {
     "Preset" = 1,
     "Custom" = 2,
     "Meme" = 3
+}
+
+export enum EmojiType {
+    "SmileysEmotion" = 1,
+    "PeopleBody" = 2,
+    "AnimalsNature" = 3,
+    "FoodDrink" = 4,
+    "TravelPlaces" = 5,
+    "Activities" = 6,
+    "Objects" = 7,
+    "Symbols" = 8,
+    "Flags" = 9
 }
