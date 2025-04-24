587b369a551acd21e3035489272b366f968e33f8
kagamaru <01a17124@gmail.com>
2025-04-24 23:08:36 +0900

    ✨ リアクションの総数ボタンを押すと、画面右側にリアクションユーザー一覧を表示する

--- src/components/atoms/TotalNumberOfReactionsButton.tsx
+++ src/components/atoms/TotalNumberOfReactionsButton.tsx
@@ -3,6 +3,7 @@ import { css } from "ss/css";
 
 type Props = {
     totalNumberOfReactions: number;
+    onClick: () => void;
 };
 
 export function TotalNumberOfReactionsButton(props: Props) {
@@ -15,7 +16,7 @@ export function TotalNumberOfReactionsButton(props: Props) {
     });
 
     return (
-        <Button type="text" className={totalNumberOfReactionsButton}>
+        <Button type="text" className={totalNumberOfReactionsButton} onClick={props.onClick}>
             <Row className={totalNumberOfReactionsButtonText}>
                 <div>{props.totalNumberOfReactions}</div>
                 <div className="ml-1">Reactions</div>
