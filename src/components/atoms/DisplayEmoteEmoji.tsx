c1b4c87c4300cc9258a433e0d477a5041eaf155b
kagamaru <01a17124@gmail.com>
2025-04-29 14:45:54 +0900

    ✨ Emojiコンポーネントを導入

--- src/components/atoms/DisplayEmoteEmoji.tsx
+++ src/components/atoms/DisplayEmoteEmoji.tsx
@@ -1,10 +1,7 @@
 import { EmojiIdObject, EmoteEmojis } from "@/@types";
-import { EmojiCategory } from "@/@types/Emoji";
 import { Row } from "antd";
-import Image from "next/image";
-import * as emojiManipulator from "node-emoji";
 import { css } from "ss/css";
-import { emojiHelper } from "@/helpers";
+import { Emoji } from "./Emoji";
 
 type Props = {
     emojis: EmoteEmojis;
@@ -16,33 +13,9 @@ export function DisplayEmoteEmoji(props: Props) {
         fontSize: "48px !important"
     });
 
-    const imageEmoji = css({
-        height: "62px !important",
-        width: "62px !important"
-    });
-
     // NOTE: 本来keyにindexを設定するのは望ましくないが、ここでは差し替えや入れ替えを伴わないためindexにする
     const displayEmojis = props.emojis.map((emoji: EmojiIdObject, index: number) => {
-        const returnedEmoji = emojiHelper(emoji.emojiId);
-
-        if (returnedEmoji.emojiCategory === EmojiCategory.Preset) {
-            return (
-                <div key={index} aria-label={emoji.emojiId}>
-                    {emojiManipulator.get(returnedEmoji.emojiId)}
-                </div>
-            );
-        } else {
-            return (
-                <Image
-                    key={index}
-                    src={returnedEmoji.url ?? ""}
-                    alt={returnedEmoji.emojiJapaneseId}
-                    width={62}
-                    height={62}
-                    className={imageEmoji}
-                />
-            );
-        }
+        return <Emoji key={index} emojiId={emoji.emojiId} size={62} />;
     });
 
     return (
