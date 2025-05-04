7a68454081519635feee385f0db2cb4a2370cf80
kagamaru <01a17124@gmail.com>
2025-05-04 16:33:04 +0900

    :recycle: 絵文字ボタンコンポーネントのリファクタリングと絵文字ヘルパーの最適化

--- src/helpers/emojiHelper.ts
+++ src/helpers/emojiHelper.ts
@@ -2,14 +2,13 @@ import { Emoji, EmojiCategory, EmojiType } from "@/@types/Emoji";
 import { EmojiString } from "@/@types/EmojiString";
 import { customEmojiMap, memeEmojiMap, presetEmojiMap } from "@/static/EmojiMap";
 
+const combinedEmojiMap = [...presetEmojiMap, ...customEmojiMap, ...memeEmojiMap];
+
 // NOTE: Emoji型を返したいため、undefinedの時は虫を返すようにして握りつぶす。
 // NOTE: 渡されるIDは全てjson内に存在するため、実際に使用した際はバグでない限りbugが帰ることはない。
 export function emojiHelper(emojiId: EmojiString): Emoji {
     return (
-        presetEmojiMap
-            .concat(customEmojiMap)
-            .concat(memeEmojiMap)
-            .find((emoji: Emoji) => emoji.emojiId === emojiId) ?? {
+        combinedEmojiMap.find((emoji: Emoji) => emoji.emojiId === emojiId) ?? {
             emojiChar: "🐛",
             emojiCategory: EmojiCategory.Preset,
             emojiType: EmojiType.AnimalsNature,
