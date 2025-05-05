008937e73e7174a543a9bf4644c10c05961df604
kagamaru <01a17124@gmail.com>
2025-05-05 11:01:25 +0900

    :sparkles: カスタム絵文字、ミーム絵文字に合わせて外観を調整

--- src/components/atoms/DisplayEmoteEmoji.tsx
+++ src/components/atoms/DisplayEmoteEmoji.tsx
@@ -9,8 +9,9 @@ type Props = {
 
 export function DisplayEmoteEmoji(props: Props) {
     const emojiRow = css({
+        marginTop: "8px",
         marginLeft: { base: "8px", lg: "0px" },
-        fontSize: "48px !important"
+        fontSize: "62px !important"
     });
 
     // NOTE: 本来keyにindexを設定するのは望ましくないが、ここでは差し替えや入れ替えを伴わないためindexにする
--- src/components/atoms/EmojiButtonRow.tsx
+++ src/components/atoms/EmojiButtonRow.tsx
@@ -1,5 +1,6 @@
 import { Col, Row } from "antd";
 import { EmojiButton } from "./EmojiButton";
+import { useIsMobile } from "@/hooks";
 import { Emoji, EmojiString } from "@/@types";
 
 type Props = {
@@ -8,10 +9,12 @@ type Props = {
 };
 
 export function EmojiButtonRow({ emojis, onClick }: Props) {
+    const isMobile = useIsMobile();
+
     return (
         <Row>
             {emojis.map(({ emojiId }) => (
-                <Col key={emojiId} span={2}>
+                <Col key={emojiId} span={isMobile ? 4 : 2}>
                     <EmojiButton emojiId={emojiId} size={24} onClick={() => onClick(emojiId)} />
                 </Col>
             ))}
