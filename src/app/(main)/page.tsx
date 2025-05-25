58d96dcac9d5c4fbe363dec16ac1c02a0e261494
kagamaru <01a17124@gmail.com>
2025-05-25 15:14:09 +0900

    :sparkles: ログイン画面ではWebSocket Connectionを貼らないようにする

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,29 +1,23 @@
 "use client";
 
-import { EmoteService, WebSocketService } from "@/app/api";
+import { useContext, useEffect } from "react";
+import { EmoteService } from "@/app/api";
 import { DisplayErrorMessage } from "@/components/atoms";
 import { PageHeader } from "@/components/molecules";
 import { WordlessEmotes } from "@/components/organisms";
 import { useError } from "@/hooks";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
 
 export default function Home() {
     const { handledError, handleErrors } = useError();
-    const idToken = localStorage.getItem("IdToken");
 
     const { data, isError, error } = useQuery({
         queryKey: ["emotes"],
         queryFn: async () => {
-            return await new EmoteService().fetchEmotes("@fuga_fuga", idToken ?? "");
+            return await new EmoteService().fetchEmotes("@fuga_fuga", localStorage.getItem("IdToken") ?? "");
         },
         retry: 0
     });
-
-    useEffect(() => {
-        const url = process.env.NEXT_PUBLIC_WEBSOCKET_URL + "?Authorization=" + idToken;
-        new WebSocketService(url, handleErrors);
-    }, []);
 
     useEffect(() => {
         if (isError && error) {
