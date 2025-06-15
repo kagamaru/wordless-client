59cdbc2ed6f036493d0db2c97432f9ada2e8cd71
kagamaru <01a17124@gmail.com>
2025-06-15 14:19:55 +0900

    :sparkles: UserSub-取得APIの呼び出し部分実装

---
+++ src/app/api/userSub/[userSub]/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { fetchWithTimeout, handleAPIError } from "@/helpers";
+
+import { User } from "@/@types";
+
+const restApiUrl = process.env.REST_API_URL ?? "";
+
+export async function GET(req: NextRequest, { params }: { params: Promise<{ userSub: string }> }) {
+    const { userSub } = await params;
+    const token = req.headers.get("authorization");
+
+    if (!userSub || !token) {
+        return NextResponse.json({ error: "Missing parameters" }, { status: 400 });
+    }
+
+    try {
+        const response = await fetchWithTimeout<User>(restApiUrl + `users/sub/${userSub}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json"
+            }
+        });
+
+        return NextResponse.json(response.data, { status: response.status });
+    } catch (error) {
+        return handleAPIError(error);
+    }
+}
