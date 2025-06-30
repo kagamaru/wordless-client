f5ef9894f6f48dbed0f02dc8c987afbaa40573ae
kagamaru <01a17124@gmail.com>
2025-06-30 21:22:19 +0900

    :sparkles: ダッシュボード画面のレイアウトを見やすくする

--- src/components/atoms/WordlessDivider.tsx
+++ src/components/atoms/WordlessDivider.tsx
@@ -1,11 +1,16 @@
 import { Divider } from "antd";
 import { css } from "ss/css";
 
-export function WordlessDivider() {
+type Props = {
+    dashed: boolean;
+};
+
+export function WordlessDivider({ dashed }: Props) {
     const dividerStyle = css({
         margin: "0px !important",
-        borderTop: "1px solid !important",
-        borderColor: "lightGrey !important"
+        borderTop: `1px ${dashed ? "dashed" : "solid"} !important`,
+        borderColor: "superLightGrey !important",
+        borderWidth: "1px !important"
     });
 
     return (
