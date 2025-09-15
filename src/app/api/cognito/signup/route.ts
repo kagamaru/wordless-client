f160f840f0d8438dee442b0ec0559636b0253317
kagamaru <01a17124@gmail.com>
2025-09-15 15:04:10 +0900

    :sparkles: ユーザー登録API の Next.js API Route呼び出し部分の実装

---
+++ src/app/api/cognito/signup/route.ts
@@ -0,0 +1,33 @@
+import { SignUpCommand } from "@aws-sdk/client-cognito-identity-provider";
+import { NextRequest, NextResponse } from "next/server";
+import { getCognitoProviderClient } from "@/app/api/cognito/getCognitoProviderClient";
+
+const cognitoClientId = process.env.COGNITO_CLIENT_ID as string;
+const sampleUserEmailNozomi = process.env.NEXT_PUBLIC_SAMPLE_USER_NOZOMI_MAIL_ADDRESS as string;
+const sampleUserEmailNico = process.env.NEXT_PUBLIC_SAMPLE_USER_NICO_MAIL_ADDRESS as string;
+
+export async function POST(req: NextRequest) {
+    const client = getCognitoProviderClient();
+    try {
+        const { email, password } = await req.json();
+
+        if (!email || !password) {
+            return NextResponse.json({ error: "Missing parameters" }, { status: 400 });
+        }
+
+        if (email === sampleUserEmailNozomi || email === sampleUserEmailNico) {
+            return NextResponse.json({ error: "Sample user cannot reset password" }, { status: 400 });
+        }
+
+        const command = new SignUpCommand({
+            ClientId: cognitoClientId,
+            Username: email,
+            Password: password
+        });
+
+        const response = await client.send(command);
+        return NextResponse.json(response, { status: 200 });
+    } catch (error) {
+        throw error;
+    }
+}
