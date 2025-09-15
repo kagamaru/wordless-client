2e831c6ebcc3ffd125a84209225b6b74960a6ff5
kagamaru <01a17124@gmail.com>
2025-09-15 15:56:41 +0900

    :sparkles: 画面からサインアップAPIを呼び出す

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,35 +1,12 @@
 "use client";
 
-import { Form, Tabs } from "antd";
+import { Tabs } from "antd";
 import { useState } from "react";
-import { BaseButton, EmailAddressInput, LinkButton, PasswordInput } from "@/components/atoms";
 import { CardPageTemplate } from "@/components/template";
-import { LoginTab } from "@/components/molecules";
+import { LoginTab, SignupTab } from "@/components/molecules";
 
 export default function LoginSignup() {
-    const [form] = Form.useForm();
     const [activeTab, setActiveTab] = useState("login");
-
-    // TODO: 後続で実装する
-    // const onSignupClick = async (email: string, password: string) => {
-    //     try {
-    //         await form.validateFields();
-    //     } catch {
-    //         return;
-    //     }
-    //     // await authService.signup(email, password);
-    // };
-
-    const signupTab = (
-        <>
-            <Form form={form}>
-                <EmailAddressInput />
-                <PasswordInput />
-                <BaseButton label="ユーザー登録" loading={false} />
-                <LinkButton label="パスワードを忘れた場合" routerPath="/auth/forgetPassword/emailAddressInput" />
-            </Form>
-        </>
-    );
 
     const tabItems = [
         {
@@ -40,15 +17,13 @@ export default function LoginSignup() {
         {
             key: "signup",
             label: "ユーザー登録",
-            children: signupTab
+            children: <SignupTab />
         }
     ];
 
     return (
-        <>
-            <CardPageTemplate>
-                <Tabs activeKey={activeTab} items={tabItems} onChange={setActiveTab} centered></Tabs>
-            </CardPageTemplate>
-        </>
+        <CardPageTemplate>
+            <Tabs activeKey={activeTab} items={tabItems} onChange={setActiveTab} centered></Tabs>
+        </CardPageTemplate>
     );
 }
