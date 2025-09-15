dd66bf8ca987477b6111f28e148a4f34e6612798
kagamaru <01a17124@gmail.com>
2025-09-15 12:00:30 +0900

    :sparkles: パスワード登録確認APIのNext.js API Route からの呼び出し部分実装

---
+++ src/app/api/cognito/confirmForgotPassword/route.ts
@@ -0,0 +1,34 @@
+import { ConfirmForgotPasswordCommand } from "@aws-sdk/client-cognito-identity-provider";
+import { NextRequest, NextResponse } from "next/server";
+import { getCognitoProviderClient } from "@/app/api/cognito/getCognitoProviderClient";
+
+const cognitoClientId = process.env.COGNITO_CLIENT_ID as string;
+const sampleUserEmailNozomi = process.env.NEXT_PUBLIC_SAMPLE_USER_NOZOMI_MAIL_ADDRESS as string;
+const sampleUserEmailNico = process.env.NEXT_PUBLIC_SAMPLE_USER_NICO_MAIL_ADDRESS as string;
+
+export async function POST(req: NextRequest) {
+    const client = getCognitoProviderClient();
+    try {
+        const { email, confirmationCode, newPassword } = await req.json();
+
+        if (!email || !confirmationCode || !newPassword) {
+            return NextResponse.json({ error: "Missing parameters" }, { status: 400 });
+        }
+
+        if (email === sampleUserEmailNozomi || email === sampleUserEmailNico) {
+            return NextResponse.json({ error: "Sample user cannot reset password" }, { status: 400 });
+        }
+
+        const command = new ConfirmForgotPasswordCommand({
+            ClientId: cognitoClientId,
+            Username: email,
+            ConfirmationCode: confirmationCode,
+            Password: newPassword
+        });
+
+        const response = await client.send(command);
+        return NextResponse.json(response, { status: 200 });
+    } catch (error) {
+        throw error;
+    }
+}
