28eaf9cb17d33edbc5e0151525f6452752261812
kagamaru <01a17124@gmail.com>
2025-09-15 21:07:16 +0900

    :sparkles: ユーザー情報登録画面を作成する

--- src/app/auth/registration/userInfo/page.tsx
+++ src/app/auth/registration/userInfo/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { Typography } from "antd";
-import { CloseTabWarning } from "@/components/atoms";
+import { Form, Typography } from "antd";
+import { BaseButton, CloseTabWarning, UserIdInput, UserNameInput } from "@/components/atoms";
 import { CardPageTemplate } from "@/components/template";
+import { css } from "ss/css";
 
 const { Title, Text } = Typography;
 
 export default function RegistrationUserInfoPage() {
+    const [form] = Form.useForm();
+
+    const inputStyle = css({
+        textAlign: "left"
+    });
+
     return (
         <CardPageTemplate>
             <Title level={2} className="mt-4">
@@ -16,9 +23,14 @@ export default function RegistrationUserInfoPage() {
                 <p>
                     <Text>ユーザーIDとユーザー名を登録してください。</Text>
                 </p>
-                <p>
+                <p className="mb-3">
                     <CloseTabWarning />
                 </p>
+                <Form className={inputStyle} form={form} onFinish={() => {}}>
+                    <UserIdInput />
+                    <UserNameInput />
+                    <BaseButton label="ユーザー登録" loading={false} />
+                </Form>
             </div>
         </CardPageTemplate>
     );
