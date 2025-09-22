6b2dc247e9e36160bd29b5e75d219cde8edb41b9
kagamaru <01a17124@gmail.com>
2025-09-22 21:58:06 +0900

    :white_check_mark: ユーザー画像変更APIを画面から呼び出すことの単体テスト実装

--- src/test/app/auth/registration/[userId]/userIcon/page.test.tsx
+++ src/test/app/auth/registration/[userId]/userIcon/page.test.tsx
@@ -22,13 +22,20 @@ vi.mock("next/navigation", () => ({
     })
 }));
 
+const mockFetchUserInfo = vi.fn();
+const mockPostUserImage = vi.fn();
 const server = setupServer(
     http.get("http://localhost:3000/api/user/:userId", () => {
+        mockFetchUserInfo();
         return HttpResponse.json({
             userId: "@user1",
             userName: "UserOne",
             userAvatarUrl: "https://image.test/user1.png"
         });
+    }),
+    http.post("http://localhost:3000/api/userImage/:userId", () => {
+        mockPostUserImage();
+        return HttpResponse.json({});
     })
 );
 
@@ -99,11 +106,102 @@ describe("初期表示時", () => {
 });
 
 describe("画像を変更するボタン押下時", () => {
-    test.todo("ローディングアイコンを表示する");
+    async function uploadImage() {
+        await user.click(await screen.findByRole("button", { name: "sync 画像を変更する" }));
+        await user.upload(
+            screen.getByTestId("file-input-change-image-button"),
+            new File(["test"], "test.png", { type: "image/png" })
+        );
+    }
 
-    test.todo("ユーザー画像登録APIを呼び出す");
+    test("ローディングアイコンを表示する", async () => {
+        rendering();
+        server.use(
+            http.post("http://localhost:3000/api/userImage/:userId", () => {
+                return new Promise(() => {}); // NOTE: 永続的にローディング状態を維持
+            })
+        );
 
-    test.todo("ユーザー情報取得APIを呼び出す");
+        await uploadImage();
+
+        await waitFor(() => {
+            expect(screen.getByRole("img", { name: "loading" })).toBeTruthy();
+        });
+    });
+
+    test("ユーザー画像登録APIを呼び出す", async () => {
+        rendering();
+
+        await uploadImage();
+
+        await waitFor(() => {
+            expect(mockPostUserImage).toHaveBeenCalled();
+        });
+    });
+
+    test("ユーザー情報取得APIを呼び出す", async () => {
+        rendering();
+
+        await uploadImage();
+
+        await waitFor(() => {
+            expect(mockFetchUserInfo).toHaveBeenCalled();
+        });
+    });
+
+    describe.each([
+        ["IMG-01", "不正なリクエストです。もう一度やり直してください。"],
+        ["IMG-02", "サンプルユーザーはプロフィール画像を変えることが出来ません。"],
+        ["IMG-03", "不正なリクエストです。もう一度やり直してください。"],
+        ["IMG-04", "不正なリクエストです。もう一度やり直してください。"],
+        ["IMG-05", "不正なリクエストです。もう一度やり直してください。"],
+        ["IMG-06", "エラーが発生しています。しばらくの間使用できない可能性があります。"],
+        ["IMG-07", "不正なリクエストです。もう一度やり直してください。"],
+        ["IMG-08", "エラーが発生しています。しばらくの間使用できない可能性があります。"],
+        ["IMG-09", "エラーが発生しています。しばらくの間使用できない可能性があります。"],
+        ["IMG-91", "不正なリクエストです。もう一度やり直してください。"],
+        ["IMG-92", "サンプルユーザーはプロフィール画像を変えることが出来ません。"],
+        ["IMG-93", "画像データがありません。"],
+        ["IMG-94", "エラーが発生しています。しばらくの間使用できない可能性があります。"]
+    ])("ユーザー画像登録APIで%sエラーが返却された時", (errorCode, errorMessage) => {
+        test("エラーメッセージを表示する", async () => {
+            server.use(
+                http.post("http://localhost:3000/api/userImage/:userId", () => {
+                    return HttpResponse.json({ data: errorCode }, { status: 400 });
+                })
+            );
+            rendering();
+
+            await uploadImage();
+
+            await waitFor(() => {
+                expect(screen.getByRole("alert")).toBeTruthy();
+                expect(within(screen.getByRole("alert")).getByText(errorMessage)).toBeTruthy();
+            });
+        });
+    });
+
+    describe.each([
+        ["USE-01", "不正なリクエストです。もう一度やり直してください。"],
+        ["USE-02", "不正なリクエストです。もう一度やり直してください。"],
+        ["USE-03", "エラーが発生しています。しばらくの間使用できない可能性があります。"]
+    ])("ユーザー情報取得APIで%sエラーが返却された時", (errorCode, errorMessage) => {
+        test("エラーメッセージを表示する", async () => {
+            rendering();
+            server.use(
+                http.get("http://localhost:3000/api/user/:userId", () => {
+                    return HttpResponse.json({ data: errorCode }, { status: 400 });
+                })
+            );
+
+            await uploadImage();
+
+            await waitFor(() => {
+                expect(screen.getByRole("alert")).toBeTruthy();
+                expect(within(screen.getByRole("alert")).getByText(errorMessage)).toBeTruthy();
+            });
+        });
+    });
 });
 
 describe("Wordlessを始めるボタン押下時", () => {
